199441d2684f0d0e5813bcd839ca4bd3eb7f5a44
Your Name <you@example.com>
2025-10-28 15:22:10 +0100

    Cleanup files

--- DbUploader.py
+++ DbUploader.py
@@ -1,4 +1,3 @@
-from pprint import pprint 
 from DbConnector import DbConnector
 from typing import Iterable, Dict, Any
 from pymongo import ASCENDING
@@ -25,7 +24,6 @@ class DbUploader:
                 [("genres", ASCENDING)],
                 [("cast.person_id", ASCENDING)],
                 [("crew.person_id", ASCENDING)],
-                [("links", ASCENDING)],
             ],
             "people": [
                 [("name", ASCENDING)],
@@ -36,10 +34,6 @@ class DbUploader:
                 [("userId", ASCENDING)],
                 [("movieId", ASCENDING)],
             ],
-            "links": [
-                [("movieId", ASCENDING)],
-                [("tmdbId", ASCENDING)],
-            ],
         }
         self.connection.ensure_indexes(spec)
 
@@ -48,76 +42,4 @@ class DbUploader:
             "movies": self.db["movies"].estimated_document_count(),
             "people": self.db["people"].estimated_document_count(),
             "ratings": self.db["ratings"].estimated_document_count(),
-            "links": self.db["links"].estimated_document_count(),
         }
-
-    def create_coll(self, collection_name):
-        collection = self.db.create_collection(collection_name)    
-        print('Created collection: ', collection)
-
-    def insert_documents(self, collection_name):
-        docs = [
-            {
-                "_id": 1,
-                "name": "Bobby",
-                "courses": 
-                    [
-                    {'code':'TDT4225', 'name': ' Very Large, Distributed Data Volumes'},
-                    {'code':'BOI1001', 'name': ' How to become a boi or boierinnaa'}
-                    ] 
-            },
-            {
-                "_id": 2,
-                "name": "Bobby",
-                "courses": 
-                    [
-                    {'code':'TDT02', 'name': ' Advanced, Distributed Systems'},
-                    ] 
-            },
-            {
-                "_id": 3,
-                "name": "Bobby",
-            }
-        ]  
-        collection = self.db[collection_name]
-        collection.insert_many(docs)
-        
-    def fetch_documents(self, collection_name):
-        collection = self.db[collection_name]
-        documents = collection.find({})
-        for doc in documents: 
-            pprint(doc)
-        
-
-    def drop_coll(self, collection_name):
-        collection = self.db[collection_name]
-        collection.drop()
-
-        
-    def show_coll(self):
-        collections = self.client['test'].list_collection_names()
-        print(collections)
-         
-
-
-def main():
-    program = None
-    try:
-        program = ExampleProgram()
-        program.create_coll(collection_name="Person")
-        program.show_coll()
-        program.insert_documents(collection_name="Person")
-        program.fetch_documents(collection_name="Person")
-        program.drop_coll(collection_name="Person")
-        # program.drop_coll(collection_name='person')
-        # program.drop_coll(collection_name='users')
-        # Check that the table is dropped
-        program.show_coll()
-    except Exception as e:
-        print("ERROR: Failed to use database:", e)
-    finally:
-        if program:
-            program.connection.close_connection()
-
-if __name__ == '__main__':
-    main()
